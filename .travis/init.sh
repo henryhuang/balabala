0febe0ac69e99b51cbd8ed6a09badc23c9633bee
henryhuang <h1886@outlook.com>
2018-04-21 18:08:10 +0800

    modified

--- .travis/init.sh
+++ .travis/init.sh
@@ -7,6 +7,8 @@ echo "Start init..."
 openssl aes-256-cbc -K $encrypted_b231c297449e_key -iv $encrypted_b231c297449e_iv -in ./.travis/deploy_key.enc -out deploy_key -d
 openssl aes-256-cbc -K $encrypted_b231c297449e_key -iv $encrypted_b231c297449e_iv -in ./.travis/server_key.enc -out server_key -d
 
+echo "Import ssh keys done"
+
 # Start SSH agent
 eval $(ssh-agent -s)
 
@@ -15,3 +17,5 @@ chmod 600 server_key
 
 ssh-add deploy_key
 ssh-add server_key
+
+echo "Finish init."
