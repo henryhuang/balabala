10636678ecddd60e763d8ee205446c85a2c18fe8
henryhuang <h1886@outlook.com>
2018-04-21 15:15:00 +0800

    add travis file

---
+++ .travis/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Start build..."
+
+# pull secret repo
---
+++ .travis/init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Start init..."
+# how to encrypt sensitive files: https://docs.travis-ci.com/user/encrypting-files/
+# import ssh keys
