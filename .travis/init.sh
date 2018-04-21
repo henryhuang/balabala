8445c28bd7b1a9d21dbfd19424adfd786af6a6aa
henryhuang <h1886@outlook.com>
2018-04-21 22:23:48 +0800

    re encrypted

--- .travis/init.sh
+++ .travis/init.sh
@@ -4,8 +4,8 @@ echo "Start init..."
 # how to encrypt sensitive files: https://docs.travis-ci.com/user/encrypting-files/
 
 # import ssh keys
-openssl aes-256-cbc -K $encrypted_b231c297449e_key -iv $encrypted_b231c297449e_iv -in ./.travis/deploy_key.enc -out deploy_key -d
-openssl aes-256-cbc -K $encrypted_b231c297449e_key -iv $encrypted_b231c297449e_iv -in ./.travis/server_key.enc -out server_key -d
+#openssl aes-256-cbc -K $encrypted_b231c297449e_key -iv $encrypted_b231c297449e_iv -in ./.travis/deploy_key.enc -out deploy_key -d
+#openssl aes-256-cbc -K $encrypted_b231c297449e_key -iv $encrypted_b231c297449e_iv -in ./.travis/server_key.enc -out server_key -d
 
 echo "Import ssh keys done"
 
