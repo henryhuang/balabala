0f30af69d3570654f733494c96b809ee47d6b607
henryhuang <h1886@outlook.com>
2018-04-22 10:20:01 +0800

    test

--- .travis/init.sh
+++ .travis/init.sh
@@ -3,12 +3,14 @@
 echo "Start init..."
 # how to encrypt sensitive files: https://docs.travis-ci.com/user/encrypting-files/
 
+echo "====1"
+cat ~/.ssh/config
+echo "====2"
+
 # import ssh keys
 openssl aes-256-cbc -K $encrypted_038f56157259_key -iv $encrypted_038f56157259_iv -in ./.travis/keys.zip.enc -out keys.zip -d
 
 unzip keys.zip
-
-echo "Import ssh keys done"
 
 # Start SSH agent
 eval $(ssh-agent -s)
@@ -19,4 +21,5 @@ chmod 600 server_key
 ssh-add deploy_key
 ssh-add server_key
 
+echo "Import ssh keys done"
 echo "Finish init."
